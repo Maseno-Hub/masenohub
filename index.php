7dba0e95a8781379872ea10dc877bac0842bc858
TypicalGeek <plenjo15@gmail.com>
2019-03-04 11:53:52 +0300

    Redesign navbar brand

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
   <meta charset="utf-8">
-  <title> Maseno ICT Club </title>
+  <title>Maseno Hub</title>
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -44,6 +44,7 @@
   <link rel="stylesheet" href="css/hover.css">
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/responsive.css">
+  <link rel="stylesheet" href="css/custom.css">
   <!-- <script type="text/javascript">
     window.onload = function(){ alert("Hi there Please not that this site is under construction get in touch with me Abraham @ abramogol@gmail.com or 0790463533 or karimkanji101@gmail.com");}
   </script> -->
@@ -75,7 +76,8 @@
               <span class="icon-bar"></span>
             </button>
           </a>
-          <a class="navbar-brand" href="index.php"><img src="img/logol.png" alt="Maseno ICT"></a>
+          <!-- <a class="navbar-brand" href="index.php"><img src="img/logol.png" alt="Maseno ICT"></a> -->
+          <a class="navbar-brand" href="index.php">MASENO HUB</a>
         </div>
         <!-- When edditing any content in the navigation. please edit this file   -->
         <?php include 'navigation.php'; ?>
