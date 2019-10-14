d07d25eb9a144d457b5baa5c3215a2107583928f
l3njo <plenjo15@gmail.com>
2019-10-14 21:32:42 +0300

    Fix head on projects.

--- projects.php
+++ projects.php
@@ -1,89 +1,10 @@
-<!DOCTYPE html>
-<html class="no-js">
-<head>
-  <meta charset="utf-8">
-  <title> Maseno ICT Club </title>
-  <meta name="description" content="">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- Favicon -->
-  <link rel="shortcut icon" type="image/png" href="img/favicon.jpg"/>
-  <!-- Google Fonts -->
-  <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300italic,400italic,700italic,400,300,700' rel='stylesheet' type='text/css'>
-  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,300,600,700' rel='stylesheet' type='text/css'>
-
-  <!-- Revolution css -->
-  <link rel="stylesheet" type="text/css" href="vendor/rs-plugin/css/settings.css" media="screen"/>
-  <link rel="stylesheet" href="vendor/rs-plugin/css/extralayer.css">
-  <!-- Flat icon css -->
-  <link rel="stylesheet" href="vendor/flat-icon/flaticon.css">
-
-  <!-- Font awesome -->
-  <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
-
-  <!-- Owl Carosel css -->
-  <link rel="stylesheet" href="vendor/owl/css/owl.carousel.css">
-  <link rel="stylesheet" href="vendor/owl/css/owl.theme.default.css">
-  <link rel="stylesheet" href="vendor/owl/css/owl.theme.css">
-
-  <!-- mmenu -->
-  <link type="text/css" rel="stylesheet" href="vendor/mmenu/css/jquery.mmenu.css" />
-
-  <!-- Bootstrap css -->
-  <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.css">
-
-  <!-- Animate css -->
-  <link rel="stylesheet" href="css/animate.css">
-
-  <!-- Custom Style css -->
-  <link rel="stylesheet" href="css/hover.css">
-  <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="css/responsive.css">
-  <link rel="stylesheet" href="css/custom.css">
-  <!-- <script type="text/javascript">
-  window.onload = function(){ alert("Hi there Please not that this site is under construction get in touch with me Abraham @ abramogol@gmail.com or 0790463533 or karimkanji101@gmail.com");}
-</script> -->
-
-
-<!--[if lt IE 9]>
-<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-<script>window.html5 || document.write('<script src="js/vendor/html5shiv.js"><\/script>')
-</script>
-<![endif]-->
-</head>
-<body>
-
-  <header  class="header-part">
-    <div id="home" class="wrapper">
-      <!-- Fixed navbar -->
-      <div class="navi navbar-default" role="navigation">
-        <div class="container">
-          <div class="navbar-header page-scroll">
-            <a href="#menu">
-              <button type="button" data-effect="st-effect-1" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-            </a>
-            <!-- <a class="navbar-brand" href="index.php"><img src="img/logol.png" alt="Maseno ICT"></a> -->
-            <a class="navbar-brand brand" href="index.php">MASENO HUB</a>
-          </div>
-          <!-- When edditing any content in the navigation. please edit this file   -->
-          <?php include 'navigation.php'; ?>
-          <!-- When edditing any content in the navigation. please edit this file -->
-        </div>
-      </div><!-- End of Nav -->
-    </div>
-  </header>
-
-
-  <!-- OUR LATEST WORKS -->
+<?php include 'header.php'; ?>
+  <!-- OUR LATEST WORK -->
   <section class="protfolio section-padding" id="product">
     <div class="container">
       <div class="row">
         <div class="wow zoomIn col-xs-12 text-center p-padding animated" style="visibility: visible; animation-name: zoomIn;">
-          <h1 class="section-title">our latest works</h1>
+          <h1 class="section-title">our latest work</h1>
           <p>
             At Maseno ICT, we are always on the move, always on the code. We strive to build better applications each time we meet and brainstorm. Here are some of the projects
             that have been completed by various ICT teams over the years.
@@ -106,8 +27,6 @@
         </div>
       </div>
     </div>
-
-
 
     <div class="row">
       <div id="grid" class="shuffle" style="position: relative; height: 600px;">
@@ -395,5 +314,3 @@
     </div><!-- /row -->
   </section><!-- /our latest works -->
   <?php include 'footer.php'; ?>
-</body>
-</html>
