b34b6dd3ac5a9a1d13f709e2bacc5b7512452b2e
l3njo <plenjo15@gmail.com>
2019-11-03 08:20:01 +0300

    Edit UI

---
+++ temp/comingsoon.php
@@ -0,0 +1,70 @@
+<!-- TODO Backend -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Coming Soon 12</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="icon" type="image/png" href="temp/images/icons/favicon.ico" />
+	<link rel="stylesheet" type="text/css" href="temp/vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="temp/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="temp/fonts/iconic/css/material-design-iconic-font.min.css">
+	<link rel="stylesheet" type="text/css" href="temp/vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="temp/vendor/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="temp/css/util.css">
+	<link rel="stylesheet" type="text/css" href="temp/css/main.css">
+</head>
+
+<body>
+	<div class="flex-w flex-str size1 overlay1">
+		<div class="flex-w flex-col-sb wsize1 bg0 p-l-65 p-t-37 p-b-50 p-r-80 respon1">
+			<div class="wrappic1">
+				<a href="javascript:back();">
+					<button class="btn btn-outline-secondary"><i class="pr-3 fa fa-arrow-left"></i>Go Back</button>
+				</a>
+			</div>
+
+			<div class="w-full flex-c-m p-t-80 p-b-90">
+				<div class="wsize2">
+					<h3 class="l1-txt1 p-b-34 respon3">On its way!</h3>
+
+					<p class="m2-txt1 p-b-46">Be the first to know when we're live!</p>
+
+					<form class="contact100-form validate-form m-t-10 m-b-10">
+						<div class="wrap-input100 validate-input m-lr-auto-lg" data-validate="Email is required: ex@abc.xyz">
+							<input class="s2-txt1 placeholder0 input100 trans-04" type="text" name="email" placeholder="Email Address">
+
+							<button class="flex-c-m ab-t-r size2 hov1">
+								<i class="zmdi zmdi-long-arrow-right fs-30 cl1 trans-04"></i>
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+
+		<div class="wsize1 simpleslide100-parent respon2">
+			<!-- TODO Unsplash -->
+			<div class="simpleslide100">
+				<div class="simpleslide100-item bg-img1" style="background-image: url('temp/images/bg01.jpg');"></div>
+				<div class="simpleslide100-item bg-img1" style="background-image: url('temp/images/bg02.jpg');"></div>
+				<div class="simpleslide100-item bg-img1" style="background-image: url('temp/images/bg03.jpg');"></div>
+			</div>
+		</div>
+	</div>
+
+	<script src="temp/vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="temp/vendor/bootstrap/js/popper.js"></script>
+	<script src="temp/vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="temp/vendor/select2/select2.min.js"></script>
+	<script src="temp/vendor/tilt/tilt.jquery.min.js"></script>
+	<script src="temp/js/main.js"></script>
+	<script>
+		function back() {
+			window.history.back();
+		}
+	</script>
+</body>
+</html>
