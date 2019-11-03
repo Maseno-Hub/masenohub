33aa566995d055dfc7e4e1af3ac2bc5ac184593c
l3njo <plenjo15@gmail.com>
2019-11-03 10:30:03 +0300

    Implement backend

--- navigation.php
+++ navigation.php
@@ -1,31 +1,33 @@
         <div id="navbar" class="navbar-collapse collapse pull-right hidden-xs">
           <ul class="nav navbar-nav navbar-right">
-            <li ><a class="page-scroll" href="./">Home</a></li>
+            <li><a class="page-scroll" href="./">Home</a></li>
             <li> <a class="page-scroll" href="index.php#about">about us</a></li>
             <li> <a class="page-scroll" href="projects.php">projects</a></li>
             <li> <a class="page-scroll" href="events.php">events</a></li>
             <li> <a class="page-scroll" href="gallery.php">resources</a></li>
             <li> <a class="page-scroll" href="team.php">team</a></li>
-            <li class="dropdown"> <a class="page-scroll drop dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" href="#blog">blog</a>
+            <li> <a class="page-scroll" href="blog.php">blog</a></li>
+            <!-- <li class="dropdown"> <a class="page-scroll drop dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" href="#blog">blog</a>
               <ul class="dropdown-menu" role="menu">
                 <li><a href="blog-full-width.php">full-width Blog</a></li>
                 <li><a href="blog-sidebar.php">sidebar blog</a></li>
               </ul>
-            </li>
-          <li><a class="page-scroll" href="index.php#contact">Contact</a></li>
-          <li><a class="page-scroll" href="blog-sidebar.php">forum</a></li>
-           <!-- Search Block -->
-          <li>
-            <i class="search fa fa-search search-btn"></i>
-            <div class="search-open" style="display:none;" >
-              <div class="input-group animated fadeInDown" >
-                <input type="text" class="form-control" placeholder="Search">
-                <span class="input-group-btn">
-                  <button class="btn-u" type="button">Go</button>
-                </span>
+            </li> -->
+            <li><a class="page-scroll" href="index.php#contact">Contact</a></li>
+            <li><a class="page-scroll" href="forum.php">forum</a></li>
+            <!-- Search Block -->
+            <li>
+              <i class="search fa fa-search search-btn"></i>
+              <div class="search-open" style="display:none;">
+                <div class="input-group animated fadeInDown">
+                  <input type="text" class="form-control" placeholder="Search">
+                  <span class="input-group-btn">
+                    <button class="btn-u" type="button">Go</button>
+                  </span>
+                </div>
               </div>
-            </div>
-          </li>
-          <!-- End Search Block -->
-        </ul>
-      </div><!--/.nav-collapse -->
+            </li>
+            <!-- End Search Block -->
+          </ul>
+        </div>
+        <!--/.nav-collapse -->
--- temp/comingsoon.php
+++ temp/comingsoon.php
@@ -1,13 +1,35 @@
-<!-- TODO Backend -->
+<?php
+if (session_status() === PHP_SESSION_NONE) session_start();
+if (!isset($_SESSION["flashes"])) $_SESSION["flashes"] = array();
+if (isset($_POST["mail"])) {
+	extract($_POST);
+
+	$db = mysqli_connect("remotemysql.com", "0Cr21YkoPg", "B5njh8K6kt", "0Cr21YkoPg", "3306") or die("Could not establish a connection to MySQL.");
+
+	$sql = "SELECT * FROM list WHERE mail = '$mail'";
+	$res = mysqli_query($db, $sql) or die(mysqli_error($db));
+	$num = mysqli_num_rows($res);
+
+	if ($num > 0) {
+		$flash = array('level' => 'warning', 'text' => 'You are already registered.');
+		array_push($_SESSION["flashes"], $flash);
+	} else {
+		$sql = "INSERT INTO list VALUES('$mail')";
+		mysqli_query($db, $sql) or die(mysqli_error($db));
+		$flash = array('level' => 'success', 'text' => 'You are now registered.');
+		array_push($_SESSION["flashes"], $flash);
+	}
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-	<title>Coming Soon 12</title>
+	<title>Coming Soon | Maseno Hub</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link rel="icon" type="image/png" href="temp/images/icons/favicon.ico" />
+	<link rel="shortcut icon" type="image/png" href="img/favicon.jpg" />
 	<link rel="stylesheet" type="text/css" href="temp/vendor/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="temp/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="temp/fonts/iconic/css/material-design-iconic-font.min.css">
@@ -20,6 +42,21 @@
 <body>
 	<div class="flex-w flex-str size1 overlay1">
 		<div class="flex-w flex-col-sb wsize1 bg0 p-l-65 p-t-37 p-b-50 p-r-80 respon1">
+
+			<?php
+			if (sizeof($_SESSION["flashes"]) > 0) :
+				foreach ($_SESSION["flashes"] as $f) : ?>
+					<div class="alert alert-<?= $f["level"]; ?> alert-dismissible fade show" role="alert">
+						<strong class="text-capitalize"><?= $f["level"]; ?>!</strong> <?= $f["text"]; ?>
+						<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+			<?php endforeach;
+				$_SESSION["flashes"] = array();
+			endif;
+			?>
+
 			<div class="wrappic1">
 				<a href="javascript:back();">
 					<button class="btn btn-outline-secondary"><i class="pr-3 fa fa-arrow-left"></i>Go Back</button>
@@ -27,14 +64,14 @@
 			</div>
 
 			<div class="w-full flex-c-m p-t-80 p-b-90">
+
 				<div class="wsize2">
 					<h3 class="l1-txt1 p-b-34 respon3">On its way!</h3>
-
 					<p class="m2-txt1 p-b-46">Be the first to know when we're live!</p>
 
-					<form class="contact100-form validate-form m-t-10 m-b-10">
+					<form class="contact100-form validate-form m-t-10 m-b-10" method="post">
 						<div class="wrap-input100 validate-input m-lr-auto-lg" data-validate="Email is required: ex@abc.xyz">
-							<input class="s2-txt1 placeholder0 input100 trans-04" type="text" name="email" placeholder="Email Address">
+							<input class="s2-txt1 placeholder0 input100 trans-04" type="text" name="mail" placeholder="Email Address">
 
 							<button class="flex-c-m ab-t-r size2 hov1">
 								<i class="zmdi zmdi-long-arrow-right fs-30 cl1 trans-04"></i>
@@ -67,4 +104,5 @@
 		}
 	</script>
 </body>
+
 </html>
